20ff691c129231f1910c68432b114554d2158bbd
krononava <lenix3347@gmail.com>
2024-05-06 14:29:04 +0800

    fix: removed unnecessary sleep

--- install-modules/gnome-terminal-customization.sh
+++ install-modules/gnome-terminal-customization.sh
@@ -5,5 +5,4 @@ source $HOME/dotfiles/install-logic/universal-install.sh
 link_file "gtk.css" "$HOME/dotfiles/" "$HOME/.config/gtk3.0/"
 
 echo -e "Restarting gnome terminal\n"
-sleep 2
 pkill gnome-terminal
