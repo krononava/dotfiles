b44b8072a7742492ba640cb694d2897d1092ae75
krononava <lenix3347@gmail.com>
2024-05-12 22:33:18 +0800

    feat: reduced timeoutlen

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -140,7 +140,7 @@ vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 50
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
