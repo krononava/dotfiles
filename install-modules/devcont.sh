4ac1918778a3787b878a757103d9725966b29591
krononava <lenix3347@gmail.com>
2024-05-22 23:18:16 +0800

    feat: copy and change directory alias for development container

--- install-modules/devcont.sh
+++ install-modules/devcont.sh
@@ -2,5 +2,6 @@
 
 source $HOME/dotfiles/install-logic/universal-install.sh
 
-add_line "alias dcb 'docker compose build'" $HOME/.config/fish/config.fish
-add_line "alias dcr 'docker compose run default'" $HOME/.config/fish/config.fish
+add_line "alias dcbuild 'docker compose build'" $HOME/.config/fish/config.fish
+add_line "alias dcrun 'docker compose run default'" $HOME/.config/fish/config.fish
+add_line "alias cpdevcont 'cp -r $HOME/dotfiles/kickstart.nvim/devcont .; cd devcont'" $HOME/.config/fish/config.fish
