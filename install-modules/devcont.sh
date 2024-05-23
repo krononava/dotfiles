8e883f7767db21b6b89ab98b208726bddf389745
krononava <lenix3347@gmail.com>
2024-05-23 13:30:21 +0800

    feat: language based development container

--- install-modules/devcont.sh
+++ install-modules/devcont.sh
@@ -3,5 +3,5 @@
 source $HOME/dotfiles/install-logic/universal-install.sh
 
 add_line "alias dcbuild 'docker compose build'" $HOME/.config/fish/config.fish
-add_line "alias dcrun 'docker compose run default'" $HOME/.config/fish/config.fish
+add_line "alias dcrun 'docker compose run'" $HOME/.config/fish/config.fish
 add_line "alias cpdevcont 'cp -r $HOME/dotfiles/kickstart.nvim/devcont .; cd devcont'" $HOME/.config/fish/config.fish
