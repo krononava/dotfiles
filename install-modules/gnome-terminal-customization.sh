aab5f2f4dbde23245e7edbca6e320eb16314e2f5
krononava <lenix3347@gmail.com>
2024-06-04 00:03:53 +0800

    feat: removed gnome customization

--- install-modules/gnome-terminal-customization.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source $HOME/dotfiles/install-logic/universal-install.sh
-
-link_file "gtk.css" "$HOME/dotfiles/" "$HOME/.config/gtk3.0/"
-
-echo -e "Restarting gnome terminal\n"
-pkill gnome-terminal
