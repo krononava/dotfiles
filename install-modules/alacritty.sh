44ea9cfceefc9a2ffc10fcea2b50a8e09821f457
krononava <lenix3347@gmail.com>
2024-06-28 15:55:26 +0800

    purge: more unneccessaries removed v2

--- install-modules/alacritty.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source $HOME/dotfiles/install-logic/universal-install.sh
-
-set_gnome_settings "org.gnome.desktop.default-applications.terminal" "exec" "alacritty"
-
-link_file "alacritty.toml" "$HOME/dotfiles/" "$HOME/.config/alacritty/"
--- install-modules/remap-capslock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source $HOME/dotfiles/install-logic/universal-install.sh
-
-add_line "XKBOPTIONS=\"ctrl:nocaps\"" "/etc/default/keyboard"
